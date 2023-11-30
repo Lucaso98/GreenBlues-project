ed4b9f87864369bb6795bb75fe87e884c4eac4d6
Kevin Roy <35423672+swebandit@users.noreply.github.com>
2023-11-30 16:47:27 +0100

    Added player animations
    Walk, idle, eat and jump are inte the files but only walk and idle work.

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -12,18 +12,22 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("move_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimationPlayer.play("Jump_Animation")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimationPlayer.play("Walk_Animation")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimationPlayer.play("Idle_Animation")
 
 	move_and_slide()
 	
@@ -31,5 +35,6 @@ func _physics_process(delta):
 
 
 func _on_area_2d_body_entered(body):
+	$AnimationPlayer.play("Eat_Animation")
 	if body.is_in_group("collectible"):
 		body.Collect()
